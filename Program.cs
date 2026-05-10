2ed593eb85e73abe4d476108514d12ef99594f12
TheLuckyRogue <spencernaugler7@gmail.com>
2026-05-10 01:07:17 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using System.Diagnostics;
+using Fluid;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services
+            .AddSingleton<FluidParser>()
+            .AddSingleton<PhysicalFileProvider>((services) =>
+            {
+                var dir = Path.Combine(Directory.GetCurrentDirectory(), "Templates");
+                var provider = new PhysicalFileProvider(dir, ExclusionFilters.None);
+                provider.Watch("*.*");
+                return provider;
+            })
+            .AddSingleton<TemplateOptions>((services) =>
+            {
+                var provider = services.GetService<PhysicalFileProvider>();
+                var options = TemplateOptions.Default;
+                options.FileProvider = provider;
+                return options;
+            })
+            .AddKeyedScoped<IFluidTemplate>("index", (IServiceProvider services, object key) =>
+            {
+                var provider = services.GetService<PhysicalFileProvider>();
+                var parser = services.GetService<FluidParser>();
+
+                var fileinfo = provider.GetFileInfo("index.liquid");
+                var text = File.ReadAllText(fileinfo?.PhysicalPath ?? string.Empty);
+
+                if (!parser.TryParse(text, out IFluidTemplate template))
+                {
+                    throw new Exception("Could not parse");
+                }
+                return template;
+            });
+
+        var app = builder.Build();
+        app.MapStaticAssets();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.MapGet("/", (context) =>
+        {
+            context.Response.Redirect("/html");
+            return Task.CompletedTask;
+        });
+
+        app.MapGet("/html", async ([FromQuery(Name = "q")] string? query, [FromKeyedServices("index")] IFluidTemplate template, TemplateOptions options) =>
+        {
+            List<string> contacts = [];
+            if (query is null)
+                contacts = FakeDb.Contacts;
+            else
+                contacts = FakeDb.Contacts
+                    .Where(contact => contact.ToUpper().Trim().Contains(query.ToString().ToUpper().Trim()))
+                    .ToList();
+
+            var context = new TemplateContext(new { Contacts = contacts }, options);
+            var final = await template.RenderAsync(context);
+
+            return Results.Content(final, "text/html");
+        });
+
+        app.Run();
+    }
+}
+
+public static class FakeDb
+{
+    public static List<string> Contacts { get; set; } = ["Ben", "James"];
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Test out a hypermedia driven application
+
+## Templating
+
+Using the Fluid library
+
+- [Library Used](https://github.com/sebastienros/fluid)
+- [Short guide](https://deanebarker.net/tech/fluid/)
+
+## Guide
+
+How we build hypermedia driven applications
+
+- [Guide](https://hypermedia.systems/book/contents/)
+  - [Bookmark](https://hypermedia.systems/a-web-1-0-application/)
