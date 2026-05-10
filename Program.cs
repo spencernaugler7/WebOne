c6bb469933e3caf8903e384562e75580f94e1fe6
TheLuckyRogue <spencernaugler7@gmail.com>
2026-05-10 01:11:54 -0400

    add namespace

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.FileProviders.Physical;
 
+namespace WebOne;
+
 public partial class Program
 {
     private static void Main(string[] args)
