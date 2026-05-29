5cdc11e17a392ea33acafe0ff1014fb1501dbd5d
TheLuckyRogue <spencernaugler7@gmail.com>
2026-05-29 00:25:29 -0400

    Add custom exception handler. Try and find out why async tasks are canceled in datastar service.

--- Program.cs
+++ Program.cs
@@ -19,12 +19,13 @@ public partial class Program
         builder.Services.AddDbContext<WebOneDbContext>();
         builder.Services.AddDatastar();
         builder.Services.AddTemplateRegistry();
+        builder.Services.AddProblemDetails();
         builder.Services.AddExceptionHandler<WebOneExceptionHandler>();
 
         var app = builder.Build();
 
         app.MapStaticAssets();
-
+        app.UseExceptionHandler();
         app.MapGet("/", (context) =>
         {
             context.Response.Redirect("/contacts");
@@ -62,16 +63,21 @@ public partial class Program
         app.Run();
     }
 
-    private class WebOneExceptionHandler(TemplateRegistry registry, IDatastarService dataStar) : IExceptionHandler
+    public sealed class WebOneExceptionHandler(TemplateRegistry registry, IServiceProvider provider) : IExceptionHandler
     {
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
+            using var scope = provider.CreateScope();
+            var dataStar = scope.ServiceProvider.GetService<IDatastarService>();
+            dataStar.ThrowIfNull("Datastar service cannot be null");
+
             var model = new
             {
                 Endpoint = httpContext.Request.GetEncodedUrl(),
                 Message = exception.ToString()
             };
             var html = await registry.RenderTemplateAsync("exception.liquid", model);
+            var options = new PatchElementsOptions();
             await dataStar.PatchElementsAsync(html, cancellationToken);
             return true;
         }
