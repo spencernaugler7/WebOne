8f25c046bb2901e9aa4ef27bd83e2e7a41ce1ad6
TheLuckyRogue <spencernaugler7@gmail.com>
2026-05-29 11:47:31 -0400

    Move exception handler to separate file.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 using DotNetEnv;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using OneOf.Types;
 using StarFederation.Datastar.DependencyInjection;
@@ -70,26 +68,5 @@ public partial class Program
         });
 
         app.Run();
-    }
-
-    public sealed class WebOneExceptionHandler(TemplateRegistry registry, IServiceProvider provider) : IExceptionHandler
-    {
-        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
-        {
-            using var scope = provider.CreateScope();
-            var dataStar = scope.ServiceProvider.GetService<IDatastarService>();
-            dataStar.ThrowIfNull("Datastar service cannot be null");
-
-            var model = new
-            {
-                ShowHomeLink = true,
-                Endpoint = httpContext.Request.GetEncodedUrl(),
-                Message = exception.ToString()
-            };
-            var html = await registry.RenderTemplateAsync("exception.liquid", model);
-            var options = new PatchElementsOptions();
-            await dataStar.PatchElementsAsync(html, cancellationToken);
-            return true;
-        }
     }
 }
